523bcef40066b73b4c72304084a8c426ec8cc64e
tatiTW <tatiana.andrade@thoughtworks.com>
2022-10-13 15:04:51 -0500

    First version api-backend

--- src/main/java/com/sampleecommerce/back/apiback/infra/outputadapter/MemoryRepository.java
+++ src/main/java/com/sampleecommerce/back/apiback/infra/outputadapter/MemoryRepository.java
@@ -19,11 +19,11 @@ public class MemoryRepository implements EntityRepository{
     public List<Procedure> getAllByApplicantId(String applicantId) {
         ArrayList<Procedure> procedures = new ArrayList<Procedure>();
         procedures.add(new Procedure(1,"a", "100","tramite1"));
-        procedures.add(new Procedure(2,"b", "100","tramite1"));
-        procedures.add(new Procedure(3,"c", "100","tramite1"));
-        procedures.add(new Procedure(4,"a", "101","tramite1"));
-        procedures.add(new Procedure(5,"b", "101","tramite1"));
-        procedures.add(new Procedure(6,"c", "101","tramite1"));
+        procedures.add(new Procedure(2,"b", "100","tramite2"));
+        procedures.add(new Procedure(3,"c", "100","tramite3"));
+        procedures.add(new Procedure(4,"a", "101","tramite4"));
+        procedures.add(new Procedure(5,"b", "101","tramite5"));
+        procedures.add(new Procedure(6,"c", "101","tramite6"));
         return procedures;
     }
 }
